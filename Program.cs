d21036119a60f614a33e4eb887c2420acafa9b00
Steven Simms <simmsstev@gmail.com>
2021-11-11 16:52:48 -0800

    Added Delete functionality to API

--- Program.cs
+++ Program.cs
@@ -8,12 +8,15 @@ builder.Services.AddDbContext<BookContext>(options => options.UseNpgsql(builder.
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Multiverse!");
-
+//------------------------------------------------
+//CRUD OPS BELOW
+//-------------------------------------------------
+//Read/GET
 app.MapGet("/api/books", async (BookContext db) => await db.Books.ToListAsync());
-
+//Read/GET
 app.MapGet("api/books/{id}", async (BookContext db, int id) => await db.Books.FindAsync(id));
 
-
+//Create/POST
 app.MapPost("/api/books", async (BookContext db, Book book) =>
 {
     await db.Books.AddAsync(book);
@@ -21,7 +24,7 @@ app.MapPost("/api/books", async (BookContext db, Book book) =>
     Results.Accepted();
 });
 
-
+//Update/Edit
 app.MapPut("api/books/{id}", async (BookContext db, int id, Book book) =>
 {
     if (id != book.Id) return Results.BadRequest();
@@ -31,4 +34,17 @@ app.MapPut("api/books/{id}", async (BookContext db, int id, Book book) =>
     return Results.NoContent();
 });
 
+
+//DELETE
+app.MapDelete("api/books/{id}", async (BookContext db, int id) =>
+{
+    var book = await db.Books.FindAsync(id);
+    if (book == null) return Results.NotFound();
+
+    db.Books.Remove(book);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+
+});
 app.Run();
