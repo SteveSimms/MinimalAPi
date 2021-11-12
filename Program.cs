7abea6f36f5ae14b14a77930979446bfa44c33be
Steven Simms <simmsstev@gmail.com>
2021-11-11 20:15:22 -0800

    refactored api

--- Program.cs
+++ Program.cs
@@ -1,23 +1,28 @@
 using MinimalAPi;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 // establishing DbContext through builder method
 builder.Services.AddDbContext<BookContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("BookDb")));
 var app = builder.Build();
-
+//MapGet uses endpoints
 app.MapGet("/", () => "Hello Multiverse!");
+var test = app.MapGet("/test", () => "Salam Sidi Asim");
+
+
+
 //------------------------------------------------
 //CRUD OPS BELOW
 //-------------------------------------------------
 //Read/GET
-app.MapGet("/api/books", async (BookContext db) => await db.Books.ToListAsync());
+var getAllBooks = app.MapGet("/api/books", async (BookContext db) => await db.Books.ToListAsync());
 //Read/GET
-app.MapGet("api/books/{id}", async (BookContext db, int id) => await db.Books.FindAsync(id));
+var getBookById = app.MapGet("api/books/{id}", async (BookContext db, int id) => await db.Books.FindAsync(id));
 
 //Create/POST
-app.MapPost("/api/books", async (BookContext db, Book book) =>
+var postBook = app.MapPost("/api/books", async (BookContext db, Book book) =>
 {
     await db.Books.AddAsync(book);
     await db.SaveChangesAsync();
@@ -25,7 +30,7 @@ app.MapPost("/api/books", async (BookContext db, Book book) =>
 });
 
 //Update/Edit
-app.MapPut("api/books/{id}", async (BookContext db, int id, Book book) =>
+var updateBook = app.MapPut("api/books/{id}", async (BookContext db, int id, Book book) =>
 {
     if (id != book.Id) return Results.BadRequest();
     db.Update(book);
@@ -36,7 +41,7 @@ app.MapPut("api/books/{id}", async (BookContext db, int id, Book book) =>
 
 
 //DELETE
-app.MapDelete("api/books/{id}", async (BookContext db, int id) =>
+var deleteBookById = app.MapDelete("api/books/{id}", async (BookContext db, int id) =>
 {
     var book = await db.Books.FindAsync(id);
     if (book == null) return Results.NotFound();
@@ -47,4 +52,22 @@ app.MapDelete("api/books/{id}", async (BookContext db, int id) =>
     return Results.NoContent();
 
 });
+
+
 app.Run();
+//builder.Services.AddAuthentication();
+
+//builder.Services.AddControllers();
+
+//app.UseEndpoints(endpoints =>
+//{
+//endpoints.MapDefaultControllerRoute();
+//});
+
+//public class TestController : Controller
+//{
+//    public IActionResult Index()
+//    {
+//        return Ok("Salam Sidi Asim");
+//    }
+//}
