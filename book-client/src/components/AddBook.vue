68583d25e3f4225c12bd4b2a18c4dc964a1b9e83
Steven Simms <simmsstev@gmail.com>
2021-11-22 19:02:17 -0800

    Wired up Submit button to form

--- book-client/src/components/AddBook.vue
+++ book-client/src/components/AddBook.vue
@@ -1,20 +1,25 @@
 ﻿<script setup>
     import { ref, computed } from 'vue'
-  /*  import { useStore } from 'vuex'*/
+    /*  import { useStore } from 'vuex'*/
     import axios from 'axios'
 
-    let isExpanded = ref(false)
-
-    const log = console.log()
 
 
+    const log = console.log
+
+    const submitBook = () => {
+        log('Book submitted')
+    }
 </script>
 
 
 
 
 <template>
-
+    <h1>Form goes here</h1>
+    <form @submit.prevent="submitBook" action="/" method="post">
+        <button class="btn btn-primary">Add Book</button>
+    </form>
 </template>
 
 
