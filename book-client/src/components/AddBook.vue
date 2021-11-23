e73c29950b3fb8898e1442352b5bf238ec33b2d3
Steven Simms <simmsstev@gmail.com>
2021-11-22 19:48:18 -0800

    Stubbed out post request and left notes for tommorrows work

--- book-client/src/components/AddBook.vue
+++ book-client/src/components/AddBook.vue
@@ -3,12 +3,31 @@
     /*  import { useStore } from 'vuex'*/
     import axios from 'axios'
 
+    //const props = defineProps({
+    //    books:
+    //})
+    const books = ref([])
 
+    //Ship THIS Object to the server
+    let formInputs = ref({
+        title: '',
+        author: '',
+        description: ''
+    })
 
     const log = console.log
 
-    const submitBook = () => {
-        log('Book submitted')
+    const submitBook = async () => {
+        //await axios.post('http://localhost:5106/api/books')
+        //    .then((resposne) => {
+        //        books.value = resposne.data
+        //        console.log('From AddBook.vue', resposne.data)
+        //        return resposne.data
+        //    })
+        formInputs.value.title
+        formInputs.value.author
+        formInputs.value.description
+        log('Book submitted', formInputs.value) //Send formInputs.value to the server
     }
 </script>
 
@@ -17,13 +36,48 @@
 
 <template>
     <h1>Form goes here</h1>
-    <form @submit.prevent="submitBook" action="/" method="post">
-        <button class="btn btn-primary">Add Book</button>
-    </form>
+    <div class="container">
+
+        <form @submit.prevent="submitBook" action="/" method="post">
+            <div class="form-group">
+                <label class="control-label">Title</label>
+                <input v-model="formInputs.title" class="form-control" />
+                <span class="text-danger"></span>
+            </div>
+            {{ formInputs.title }}
+            <div class="form-group">
+                <label class="control-label">Author</label>
+                <input v-model="formInputs.author" class="form-control" />
+                <span class="text-danger"></span>
+            </div>
+            {{ formInputs.author }}
+            <div class="form-group">
+                <label class="control-label">Description</label>
+                <textarea v-model="formInputs.description" class="form-control"> </textarea>
+                <span class="text-danger"></span>
+            </div>
+            {{ formInputs.description }}
+
+            <button class="btn btn-primary">Add Book</button>
+        </form>
+
+
+    </div>
+
+
+
+
+
+
 </template>
 
 
 
-
+<style>
+    /*    input {
+        position: center;
+        width: 50%;
+    }*/
+</style>
 
 
